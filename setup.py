a6f75b37ab24c8b38bec3eaee84f6e7538b945dd
ryan <ryanyt@hawaii.edu>
2018-05-11 14:11:32 -1000

    #9 updated code that used 'occquse' to use 'survey_display' instead

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from setuptools import setup
 
 package = {
-    "name": 'occquse',
-    "packages": ['occquse'],
+    "name": 'survey_display',
+    "packages": ['survey_display'],
     "include_package_data": True,
     "install_requires": ['flask','toml']
 }
