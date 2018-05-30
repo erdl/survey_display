a34159d387956297ec16b2efcedb22a09324d5da
ryan <ryanyt@hawaii.edu>
2018-05-30 09:46:30 -1000

    #43 added database insertion code to survey POST route

--- survey_display/application.py
+++ survey_display/application.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from . import core
 from .utils import psql
+from .utils import data as data_utils
 import flask
 
 
@@ -19,9 +20,14 @@ def callback(survey):
     if flask.request.method == 'GET':
         return core.handle_spec_request(survey)
     else:
-        data = flask.request.json
-        print('data: ',data)
-        core.save_responses(survey,data)
+        data = flask.request.json[0]
+        print('Data Received: \n', data)
+
+        # Convert the responses returned by the POST request into a list of
+        # rows that can then be inserted into the database.
+
+        psql.insert_responses(data_utils.convert_response(data))
+
         return flask.jsonify({})
 
 
